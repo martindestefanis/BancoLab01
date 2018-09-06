c06abeac8312a4cefb593d3025d307dd5aa96b2e
martindestefanis <tinchodestefanis1@gmail.com>
2018-09-06 17:23:18 -0300

    Validación cuit y email en chbAceptoTerminos

--- feature/src/main/java/ar/edu/utn/frsf/isi/dam/bancolab01/feature/MainActivity.java
+++ feature/src/main/java/ar/edu/utn/frsf/isi/dam/bancolab01/feature/MainActivity.java
@@ -1,6 +1,8 @@
 package ar.edu.utn.frsf.isi.dam.bancolab01.feature;
 
 import android.graphics.Color;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -13,6 +15,7 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import ar.edu.utn.frsf.isi.dam.bancolab01.modelo.Cliente;
 import ar.edu.utn.frsf.isi.dam.bancolab01.modelo.PlazoFijo;
 
@@ -20,13 +23,18 @@ public class MainActivity extends AppCompatActivity {
 
     private PlazoFijo pf;
     private Cliente cliente;
-    private EditText edtMail, edtCuit, edtMonto;
+    private EditText edtMail;
+    private EditText edtCuit;
+    private EditText edtMonto;
     private Button btnHacerPlazoFijo;
     private SeekBar seekBar;
-    private TextView tvDiasSeleccionados, tvInteres, tvMensajes;
+    private TextView tvDiasSeleccionados;
+    private TextView tvInteres;
     private CheckBox chbAceptoTerminos;
-    int progress = 0;
+    private TextView tvMensajes;
+    int progress = 10;
 
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,14 +44,17 @@ public class MainActivity extends AppCompatActivity {
         edtMail = (EditText) findViewById(R.id.edtMail);
         edtCuit = (EditText) findViewById(R.id.edtCuit);
         edtMonto = (EditText) findViewById(R.id.edtMonto);
-        edtMonto.addTextChangedListener(new TextWatcher() {
+        edtMonto.addTextChangedListener(new TextWatcher(){
+
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
+
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) { }
+
             @Override
             public void afterTextChanged(Editable s) {
-                if(edtMonto.getText().toString().isEmpty()) {
+                if(edtMonto.getText().toString().isEmpty()){
                     pf.setMonto(0.0);
                     tvInteres.setText("$0.0");
                 }
@@ -62,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar = (SeekBar) findViewById(R.id.seekBar);
         seekBar.setMax(180);
+        //seekBar.setMin(10);
         seekBar.setProgress(progress);
 
         tvDiasSeleccionados = (TextView) findViewById(R.id.tvDiasSeleccionados);
@@ -79,7 +91,7 @@ public class MainActivity extends AppCompatActivity {
                 progress = i;
                 tvDiasSeleccionados.setText(progress + " dias de plazo");
                 pf.setDias(progress);
-                if(edtMonto.getText().toString().isEmpty()) {
+                if(edtMonto.getText().toString().isEmpty()){
                     pf.setMonto(0.0);
                     tvInteres.setText("$0.0");
                 }
@@ -113,22 +125,25 @@ public class MainActivity extends AppCompatActivity {
         btnHacerPlazoFijo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(cliente.getMail().toString().isEmpty() || cliente.getCuit().toString().isEmpty() || pf.getMonto()<=0.0 || pf.getDias()<=10) {
+                cliente.setMail(edtMail.getText().toString());
+                cliente.setCuil(edtCuit.getText().toString());
+            if(cliente.getMail().toString().isEmpty() || cliente.getCuit().toString().isEmpty() || pf.getMonto()<=0.0 || pf.getDias()<=10)
+                {
                     Toast.makeText(getApplicationContext(),"Revise los datos ingresados", Toast.LENGTH_LONG).show();
                     tvMensajes.setText("Los datos ingresados son incorrectos o no son válidos");
                     tvMensajes.setTextColor(Color.RED);
                 }
-                else {
-                    cliente.setMail(edtMail.toString());
-                    cliente.setCuil(edtCuit.toString());
-                    tvMensajes.setText("El plazo fijo se realizó correctamente \n" +
-                            "Datos del plazo fijo:\n" +
-                            "Dias: " + pf.getDias() + "\n" +
-                            "Monto: " + pf.getMonto() + "\n"+
-                            "Intereses:" + pf.intereses() + "\n");
-                    tvMensajes.setTextColor(Color.BLUE);
+            else {
+                cliente.setMail(edtMail.toString());
+                cliente.setCuil(edtCuit.toString());
+                tvMensajes.setText("El plazo fijo se realizó correctamente \n" +
+                                    "Datos del plazo fijo:\n" +
+                                    "Dias: " + pf.getDias() + "\n" +
+                                    "Monto: " + pf.getMonto() + "\n"+
+                                    "Intereses:" + pf.intereses() + "\n");
+                tvMensajes.setTextColor(Color.BLUE);
                 }
             }
         });
+        }
     }
-}
--- feature/src/main/java/ar/edu/utn/frsf/isi/dam/bancolab01/modelo/Cliente.java
+++ feature/src/main/java/ar/edu/utn/frsf/isi/dam/bancolab01/modelo/Cliente.java
@@ -1,8 +1,8 @@
 package ar.edu.utn.frsf.isi.dam.bancolab01.modelo;
 
 public class Cliente {
-    private String mail="";
-    private String cuit="";
+    private String mail;
+    private String cuit;
 
     public String getMail() {
         return mail;
