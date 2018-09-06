d57c5f046114dfd2ab0daa6640b5db4e74e9e0fb
Pedro Capdevila <pedro.capdevila@hotmail.com>
2018-09-06 17:39:06 -0300

    Corrección inicio de seek bar y corrección mensajes de error

--- feature/src/main/java/ar/edu/utn/frsf/isi/dam/bancolab01/feature/MainActivity.java
+++ feature/src/main/java/ar/edu/utn/frsf/isi/dam/bancolab01/feature/MainActivity.java
@@ -1,8 +1,6 @@
 package ar.edu.utn.frsf.isi.dam.bancolab01.feature;
 
 import android.graphics.Color;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -15,7 +13,6 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import ar.edu.utn.frsf.isi.dam.bancolab01.modelo.Cliente;
 import ar.edu.utn.frsf.isi.dam.bancolab01.modelo.PlazoFijo;
 
@@ -23,18 +20,13 @@ public class MainActivity extends AppCompatActivity {
 
     private PlazoFijo pf;
     private Cliente cliente;
-    private EditText edtMail;
-    private EditText edtCuit;
-    private EditText edtMonto;
+    private EditText edtMail, edtCuit, edtMonto;
     private Button btnHacerPlazoFijo;
     private SeekBar seekBar;
-    private TextView tvDiasSeleccionados;
-    private TextView tvInteres;
+    private TextView tvDiasSeleccionados, tvInteres, tvMensajes;
     private CheckBox chbAceptoTerminos;
-    private TextView tvMensajes;
-    int progress = 10;
+    int progress = 0;
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,17 +36,14 @@ public class MainActivity extends AppCompatActivity {
         edtMail = (EditText) findViewById(R.id.edtMail);
         edtCuit = (EditText) findViewById(R.id.edtCuit);
         edtMonto = (EditText) findViewById(R.id.edtMonto);
-        edtMonto.addTextChangedListener(new TextWatcher(){
-
+        edtMonto.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) { }
-
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) { }
-
             @Override
             public void afterTextChanged(Editable s) {
-                if(edtMonto.getText().toString().isEmpty()){
+                if(edtMonto.getText().toString().isEmpty()) {
                     pf.setMonto(0.0);
                     tvInteres.setText("$0.0");
                 }
@@ -73,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
         seekBar = (SeekBar) findViewById(R.id.seekBar);
         seekBar.setMax(180);
-        //seekBar.setMin(10);
         seekBar.setProgress(progress);
 
         tvDiasSeleccionados = (TextView) findViewById(R.id.tvDiasSeleccionados);
@@ -91,7 +79,7 @@ public class MainActivity extends AppCompatActivity {
                 progress = i;
                 tvDiasSeleccionados.setText(progress + " dias de plazo");
                 pf.setDias(progress);
-                if(edtMonto.getText().toString().isEmpty()){
+                if(edtMonto.getText().toString().isEmpty()) {
                     pf.setMonto(0.0);
                     tvInteres.setText("$0.0");
                 }
@@ -115,7 +103,7 @@ public class MainActivity extends AppCompatActivity {
                 if(chbAceptoTerminos.isChecked()) {
                     btnHacerPlazoFijo.setEnabled(true);
                 }
-                else{
+                else {
                     Toast.makeText(getApplicationContext(),"Es obligatorio aceptar los términos y condiciones",Toast.LENGTH_LONG).show();
                     btnHacerPlazoFijo.setEnabled(false);
                 }
@@ -127,21 +115,39 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 cliente.setMail(edtMail.getText().toString());
                 cliente.setCuil(edtCuit.getText().toString());
-            if(cliente.getMail().toString().isEmpty() || cliente.getCuit().toString().isEmpty() || pf.getMonto()<=0.0 || pf.getDias()<=10)
-                {
-                    Toast.makeText(getApplicationContext(),"Revise los datos ingresados", Toast.LENGTH_LONG).show();
-                    tvMensajes.setText("Los datos ingresados son incorrectos o no son válidos");
-                    tvMensajes.setTextColor(Color.RED);
-                }
+            if(cliente.getMail().toString().isEmpty()) {
+                Toast.makeText(getApplicationContext(),"Revise los datos ingresados", Toast.LENGTH_LONG).show();
+                tvMensajes.setText("Los datos ingresados son incorrectos o no son válidos: \n" +
+                        "Debe completar su e-mail.");
+                tvMensajes.setTextColor(Color.RED);
+            }
+            else if (cliente.getCuit().toString().isEmpty()) {
+                Toast.makeText(getApplicationContext(),"Revise los datos ingresados", Toast.LENGTH_LONG).show();
+                tvMensajes.setText("Los datos ingresados son incorrectos o no son válidos: \n" +
+                        "Debe completar su CIUT/CUIL.");
+                tvMensajes.setTextColor(Color.RED);
+            }
+            else if (pf.getMonto()<=0.0) {
+                Toast.makeText(getApplicationContext(),"Revise los datos ingresados", Toast.LENGTH_LONG).show();
+                tvMensajes.setText("Los datos ingresados son incorrectos o no son válidos: \n " +
+                        "El monto debe ser mayor a cero.");
+                tvMensajes.setTextColor(Color.RED);
+            }
+            else if (pf.getDias()<=10) {
+                Toast.makeText(getApplicationContext(),"Revise los datos ingresados", Toast.LENGTH_LONG).show();
+                tvMensajes.setText("Los datos ingresados son incorrectos o no son válidos: \n" +
+                        "El plazo debe ser mayor a 10 días.");
+                tvMensajes.setTextColor(Color.RED);
+            }
             else {
                 tvMensajes.setText("El plazo fijo se realizó correctamente \n" +
-                                    "Datos del plazo fijo:\n" +
-                                    "Dias: " + pf.getDias() + "\n" +
-                                    "Monto: " + pf.getMonto() + "\n"+
-                                    "Intereses:" + pf.intereses() + "\n");
+                        "Datos del plazo fijo:\n" +
+                        "Dias: " + pf.getDias() + "\n" +
+                        "Monto: " + pf.getMonto() + "\n"+
+                        "Intereses:" + pf.intereses() + "\n");
                 tvMensajes.setTextColor(Color.BLUE);
-                }
+            }
             }
         });
-        }
     }
+}
